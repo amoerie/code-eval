fd6295cba6efd8047c3f660415c0c308189e05c0
Alexander Moerman <alexander.moerman@gmail.com>
2014-08-13 14:58:13 +0200

    Added solution for TextDollar

---
+++ CodeEval.Tests/TestsForTextDollar.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CodeEval.Tests
+{
+    [TestFixture]
+    public class TestsForTextDollar
+    {
+        private TextDollar _textDollar;
+
+        [SetUp]
+        public void Setup()
+        {
+            _textDollar = new TextDollar();
+        }
+
+        [Test]
+        public void TestCases()
+        {
+            Assert.That(_textDollar.Print(3), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(10), Is.EqualTo("TenDollars"));
+            Assert.That(_textDollar.Print(21), Is.EqualTo("TwentyOneDollars"));
+            Assert.That(_textDollar.Print(466), Is.EqualTo("FourHundredSixtySixDollars"));
+            Assert.That(_textDollar.Print(1234), Is.EqualTo("OneThousandTwoHundredThirtyFourDollars"));
+            Assert.That(_textDollar.Print(101), Is.EqualTo("OneHundredOneDollars"));
+            Assert.That(_textDollar.Print(987654321), Is.EqualTo("NineHundredEightySevenMillionSixHundredFiftyFourThousandThreeHundredTwentyOneDollars"));
+        }
+
+        [Test]
+        public void HardTestCases()
+        {
+            Assert.That(_textDollar.Print(128), Is.EqualTo("OneHundredTwentyEightDollars"));
+            Assert.That(_textDollar.Print(645), Is.EqualTo("SixHundredFortyFiveDollars"));
+            Assert.That(_textDollar.Print(457), Is.EqualTo("FourHundredFiftySevenDollars"));
+            Assert.That(_textDollar.Print(534067757), Is.EqualTo("FiveHundredThirtyFourMillionSixtySevenThousandSevenHundredFiftySevenDollars"));
+            Assert.That(_textDollar.Print(968298504), Is.EqualTo("NineHundredSixtyEightMillionTwoHundredNinetyEightThousandFiveHundredFourDollars"));
+            Assert.That(_textDollar.Print(109), Is.EqualTo("OneHundredNineDollars"));
+            Assert.That(_textDollar.Print(34369), Is.EqualTo("ThirtyFourThousandThreeHundredSixtyNineDollars"));
+            Assert.That(_textDollar.Print(327), Is.EqualTo("ThreeHundredTwentySevenDollars"));
+            /*Assert.That(_textDollar.Print(252), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(123456789), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(92065), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(68), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(425), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(81), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(34590), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(654158870), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(24537), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(738091870), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(920325638), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(992947960), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(748208772), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(96108), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(402), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(1), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(75969), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(509), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(312056224), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(627), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(396), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(46601), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(16591), Is.EqualTo("ThreeDollars"));*/
+            Assert.That(_textDollar.Print(100000000), Is.EqualTo("OneHundredMillionDollars"));
+            /*Assert.That(_textDollar.Print(786), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(303483690), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(620140812), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(875), Is.EqualTo("ThreeDollars"));*/
+            Assert.That(_textDollar.Print(99080), Is.EqualTo("NinetyNineThousandEightyDollars"));
+            /*Assert.That(_textDollar.Print(561), Is.EqualTo("ThreeDollars"));
+            Assert.That(_textDollar.Print(413), Is.EqualTo("ThreeDollars"));*/
+        }
+    }
+}
---
+++ CodeEval/TextDollar.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CodeEval
+{
+    public class TextDollar
+    {
+        public static void Main(string[] args)
+        {
+            var textDollar = new TextDollar();
+            foreach (var line in Utilities.ReadLines(args[0]))
+            {
+                Console.WriteLine(textDollar.Print(Convert.ToInt64(line)));
+            }
+        }
+
+        private readonly IDictionary<long, string> _units = new Dictionary<long, string>
+        {
+            { 1, "One" },
+            { 2, "Two" },
+            { 3, "Three" },
+            { 4, "Four" },
+            { 5, "Five" },
+            { 6, "Six" },
+            { 7, "Seven" },
+            { 8, "Eight" },
+            { 9, "Nine" },
+            { 10, "Ten" },
+            { 11, "Eleven" },
+            { 12, "Twelve" },
+            { 13, "Thirteen" },
+            { 14, "Fourteen" },
+            { 15, "Fifteen" },
+            { 16, "Sixteen" },
+            { 17, "Seventeen" },
+            { 18, "Eighteen" },
+            { 19, "Nineteen" },
+        };
+
+        private readonly IDictionary<long, string> _tens = new Dictionary<long, string>
+        {
+            { 10, "Ten" },
+            { 20, "Twenty" },
+            { 30, "Thirty" },
+            { 40, "Forty" },
+            { 50, "Fifty" },
+            { 60, "Sixty" },
+            { 70, "Seventy" },
+            { 80, "Eighty" },
+            { 90, "Ninety" },
+        };
+
+        private readonly IDictionary<long, string> _powers = new Dictionary<long, string>
+        {
+            { 100, "Hundred" },
+            { 1000, "Thousand" },
+            { 1000000, "Million" },
+        };
+
+        public string Print(long amount)
+        {
+            return NumberToString(amount) + "Dollars";
+        }
+
+        private string NumberToString(long number)
+        {
+            if (number < 1)
+                return string.Empty;
+            if (number < 20)
+                return _units[number];
+            if (number < 100)
+            {
+                long remainder = number % 10;
+                return _tens[number - remainder] + NumberToString(remainder);
+            }
+            if (number < 1000)
+            {
+                long remainder = number % 100;
+                long size = number / 100;
+                return NumberToString(size) + _powers[100] + NumberToString(remainder);
+            }
+            if (number < 1000000)
+            {
+                long remainder = number % 1000;
+                long size = number / 1000;
+                return NumberToString(size) + _powers[1000] + NumberToString(remainder);
+            }
+            else
+            {
+                long remainder = number % 1000000;
+                long size = number / 1000000;
+                return NumberToString(size) + _powers[1000000] + NumberToString(remainder);
+            }
+        }
+
+        public static class Utilities
+        {
+            public static IEnumerable<string> ReadLines(string file)
+            {
+                using (var reader = File.OpenText(file))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        if (null == line)
+                            continue;
+                        yield return line;
+                    }
+                }
+            }
+        }
+    }
+}
